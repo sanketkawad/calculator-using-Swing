141377ead9dc2394d8302c7bb4c80dff2289552e
sanket kawad <sanket kawad@RadZEN.(none)>
2022-10-23 11:58:11 +0530

    calculator

---
+++ src/GUI/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author sanket kawad
+ */
+import javax.swing.*;
+
+public class MainClass {
+    public static void main(String[] args)
+    {
+            new Calculator();
+    }
+}
+
